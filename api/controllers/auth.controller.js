9fdaf7231155799c9f96bd5c5b7285b1d38b18c6
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-11 13:30:56 +0530

    Create middle-where function to handle the errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,14 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
-export const signup = async(req,res) => {
+import { errorHandler } from '../utils/error.js';
+export const signup = async(req,res,next) => {
     const {username,email,password} = req.body;
     const hashPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password: hashPassword});
     try{
         await newUser.save();
         res.status(201).json("user created successfully");
-    }catch(err){
-        res.status(500).json(err.message);
-    }
-    
+    } catch (err){
+        next(err);
+    }  
 };
