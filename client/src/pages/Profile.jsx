023f5e2a250847b49b1d87db236c409c51535344
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-15 12:50:59 +0530

    Complete ceate listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ import { updateUserStart,
   signOutUserSuccess,
   signOutUserFailure} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 export default function Profile() {
@@ -194,6 +195,12 @@ export default function Profile() {
       disabled: opacity-80'>
         {loading ? 'Loading...': 'Update'}
       </button>
+      <Link to='/create-listing' 
+      className='bg-green-700 text-white 
+      p-3 rounded-lg uppercase text-center 
+      hover:opacity-95'>
+          Create Listing
+      </Link>
 
       </form>
 
