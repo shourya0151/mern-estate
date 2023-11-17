490b1ed14a7df1086b1c75b636ba6f5e8fd5d3d3
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-17 14:55:13 +0530

    Complete listing and contact functionality and ui

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import {deleteUser, getUserListings, test, updateUser} from '../controllers/user.controller.js';
+import {deleteUser, getUserListings, test, updateUser,getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -14,5 +14,5 @@ router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
 
 router.get('/listings/:id',verifyToken,getUserListings);
-
+router.get('/:id',verifyToken,getUser);
 export default router;
