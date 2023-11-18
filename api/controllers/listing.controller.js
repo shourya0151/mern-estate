1f9769c61c46238f4941cf536531068f5ab88195
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-18 09:33:50 +0530

    Create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -65,3 +65,59 @@ export const getListing = async(req,res,next) => {
         next(error);
     }
 };
+
+//search
+
+export const getListings = async (req,res,next) => {
+    try{
+
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+
+        if(offer === undefined || offer === 'false'){
+            offer = { $in: [false,true] }; //search in data base for both true and false
+        }
+
+        let furnished = req.query.furnished;
+
+        if(furnished === undefined || furnished === 'false'){
+            furnished = { $in: [false,true] }; //search in data base for both true and false
+        }
+
+
+        let parking = req.query.parking;
+
+        if(parking === undefined || parking === 'false'){
+            parking = { $in: [false,true] }; //search in data base for both true and false
+        }
+
+        let type = req.query.type;
+
+        if(type === undefined || type === 'all'){
+            type = { $in: ['sale','rent'] }; //search in data base for both true and false
+        }
+
+        const searchTerm = req.query.searchterm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listing = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' }, //i to ignore case sensitivity
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+
+        return res.status(200).json(listing);
+        
+    }catch (error){
+        next(error);
+    }
+}
